5d22fdda33577c6e7811e140948bfe8208da129a
Алексей <skornyakov.07@inbox.ru>
2026-05-09 21:54:26 +0500

    pep8

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import get_user_model
+﻿from django.contrib.auth import get_user_model
 from django.db import models
 
 LIMIT_TEXT = 20
@@ -20,15 +20,19 @@ class Group(models.Model):
 
 class Post(models.Model):
     text = models.TextField()
-    pub_date = models.DateTimeField("Дата публикации", auto_now_add=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="posts"
+    pub_date = models.DateTimeField(
+        'Дата публикации', auto_now_add=True
     )
-    image = models.ImageField(upload_to="posts/", null=True, blank=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='posts'
+    )
+    image = models.ImageField(
+        upload_to='posts/', null=True, blank=True
+    )
     group = models.ForeignKey(
         Group,
         on_delete=models.SET_NULL,
-        related_name="posts",
+        related_name='posts',
         blank=True,
         null=True
     )
@@ -39,14 +43,14 @@ class Post(models.Model):
 
 class Comment(models.Model):
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="comments"
+        User, on_delete=models.CASCADE, related_name='comments'
     )
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments"
+        Post, on_delete=models.CASCADE, related_name='comments'
     )
     text = models.TextField()
     created = models.DateTimeField(
-        "Дата добавления", auto_now_add=True, db_index=True
+        'Дата добавления', auto_now_add=True, db_index=True
     )
 
     def __str__(self):
